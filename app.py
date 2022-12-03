12e123c2a29b4a329ece5352e9052989c342ab53
RajKulk16 <rsk132302@gmail.com>
2022-12-03 18:18:20 +0530

    3rd commit with inputs from html

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def home():
     return render_template('home.html') #index page
 
 
-@app.route('/predict_api', methods=['POST']) #using APIs for production
+@app.route('/predict_api', methods=['POST']) #using APIs for getting values
 def predict_api():
     data = request.json['data'] #key-value dictionary format
     print(data)
-    del data['TAX']
+    del data['TAX'] #removing tax as it is highly correlated to RAD
     print(np.array(list(data.values())).reshape(1,-1))
     # Need not "compulsory" do the scaling values. This is because we are using RandomForest model.
     new_data = np.array(list(data.values())).reshape(1,-1)
@@ -24,6 +24,21 @@ def predict_api():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST']) # to predict by getting input from user in the html page
+def predict():
+    data = [float(x) for x in request.form.values()]
+    del data[9] #removing tax as it is highly correlated to RAD
+    final_data = np.array((data)).reshape(1,-1) #reshaped data
+    print(final_data)
+    output = model.predict(final_data)
+    for i in output:
+        final_output = round(i,3)
+    return render_template("home.html",prediction_text ="Price of the house is approximately = {}".format(final_output))
+
+
+
+
 
 
 if __name__=="__main__":
